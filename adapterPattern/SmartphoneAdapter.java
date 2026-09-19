2d50f8b92d45e3da290dcb47d45a4cb4c5584e16
Joey Gerodias Jr. <101050559+JoeyGerodiasJr@users.noreply.github.com>
2026-09-19 21:37:50 +0800

    Update SmartphoneAdapter.java

--- adapterPattern/SmartphoneAdapter.java
+++ adapterPattern/SmartphoneAdapter.java
@@ -1,31 +1,11 @@
 package adapterPattern;
 
-public class SmartphoneAdapter implements RemoteControllerFunction {
+public class SmartphoneAdapter implements PowerOutlet {
 
     private SmartphoneCharger smartphoneCharger;
 
     public SmartphoneAdapter(SmartphoneCharger smartphoneCharger) {
         this.smartphoneCharger = smartphoneCharger;
-    }
-
-    @Override
-    public String powerOn() {
-        return "Smartphone charger power is on.";
-    }
-
-    @Override
-    public String powerOff() {
-        return "Smartphone charger power is off.";
-    }
-
-    @Override
-    public String pressPlusButton() {
-        return "Smartphone charging power increased.";
-    }
-
-    @Override
-    public String pressMinusButton() {
-        return "Smartphone charging power decreased.";
     }
 
     @Override
