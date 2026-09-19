68231076b542cd15ceaa81f627c18e56895cd0c7
Joey Gerodias Jr. <101050559+JoeyGerodiasJr@users.noreply.github.com>
2026-09-19 21:36:12 +0800

    Update ApplianceApp to integrate new device adapters
    Refactor appliance control to use adapters for new devices.

--- adapterPattern/ApplianceApp.java
+++ adapterPattern/ApplianceApp.java
@@ -17,7 +17,6 @@ public class ApplianceApp {
         System.out.println(tvRc.powerOn());
         System.out.println(airConRc.powerOn());
         System.out.println(speakerRc.powerOn());
-
         System.out.println();
 
         //press button up
@@ -25,17 +24,27 @@ public class ApplianceApp {
         System.out.println(airConRc.pressPlusButton());
         System.out.println(speakerRc.pressPlusButton());
 
-         // Plug in New Devices
+        // New devices
         Laptop laptop = new Laptop();
-        RemoteControllerFunction laptopRc = new LaptopAdapter(laptop);
+        LaptopAdapter laptopAdapter = new LaptopAdapter(laptop);
         Refrigerator refrigerator = new Refrigerator();
-        RemoteControllerFunction refrigeratorRc = new RefrigeratorAdapter(refrigerator);
+        RefrigeratorAdapter refrigeratorAdapter = new RefrigeratorAdapter(refrigerator);
         SmartphoneCharger smartphoneCharger = new SmartphoneCharger();
-        RemoteControllerFunction smartphoneRc = new SmartphoneAdapter(smartphoneCharger);
+        SmartphoneAdapter smartphoneAdapter = new SmartphoneAdapter(smartphoneCharger);
+
+        // Use RemoteControllerFunctio
+        RemoteControllerFunction refrigeratorRc = refrigeratorAdapter;
+        System.out.println("New device controls:");
+        System.out.println(refrigeratorRc.powerOn());
         System.out.println();
+
+        // Use PowerOutlet
+        PowerOutlet laptopOutlet = laptopAdapter;
+        PowerOutlet refrigeratorOutlet = refrigeratorAdapter;
+        PowerOutlet smartphoneOutlet = smartphoneAdapter;
         System.out.println("Plugging devices into the power outlet:");
-        System.out.println(laptopRc.plugIn());
-        System.out.println(refrigeratorRc.plugIn());
-        System.out.println(smartphoneRc.plugIn());
+        System.out.println(laptopOutlet.plugIn());
+        System.out.println(refrigeratorOutlet.plugIn());
+        System.out.println(smartphoneOutlet.plugIn());
     }
 }
