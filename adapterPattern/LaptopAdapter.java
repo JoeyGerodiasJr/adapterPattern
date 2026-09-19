42489e53f244f3436f9bbfe06db378febc5ec9ba
Joey Gerodias Jr. <101050559+JoeyGerodiasJr@users.noreply.github.com>
2026-09-19 21:36:32 +0800

    Update LaptopAdapter.java

--- adapterPattern/LaptopAdapter.java
+++ adapterPattern/LaptopAdapter.java
@@ -1,33 +1,13 @@
 package adapterPattern;
 
-public class LaptopAdapter implements RemoteControllerFunction {
+public class LaptopAdapter implements PowerOutlet {
 
     private Laptop laptop;
 
     public LaptopAdapter(Laptop laptop) {
         this.laptop = laptop;
     }
-
-    @Override
-    public String powerOn() {
-        return "Laptop power is on.";
-    }
-
-    @Override
-    public String powerOff() {
-        return "Laptop power is off.";
-    }
-
-    @Override
-    public String pressPlusButton() {
-        return "Laptop volume or brightness increased.";
-    }
-
-    @Override
-    public String pressMinusButton() {
-        return "Laptop volume or brightness decreased.";
-    }
-
+    
     @Override
     public String plugIn() {
         return laptop.charge();
