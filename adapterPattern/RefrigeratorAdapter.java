365dce29eeb438f2aaa0496b2876ca5ab2ded067
Joey Gerodias Jr. <101050559+JoeyGerodiasJr@users.noreply.github.com>
2026-09-19 21:49:30 +0800

    Refactor RefrigeratorAdapter by removing unused methods
    Removed power control and cooling adjustment methods.

--- adapterPattern/RefrigeratorAdapter.java
+++ adapterPattern/RefrigeratorAdapter.java
@@ -9,26 +9,6 @@ public class RefrigeratorAdapter implements PowerOutlet, RemoteControllerFunctio
     }
 
     @Override
-    public String powerOn() {
-        return "Refrigerator power is on.";
-    }
-
-    @Override
-    public String powerOff() {
-        return "Refrigerator power is off.";
-    }
-
-    @Override
-    public String pressPlusButton() {
-        return "Refrigerator cooling increased.";
-    }
-
-    @Override
-    public String pressMinusButton() {
-        return "Refrigerator cooling decreased.";
-    }
-
-    @Override
     public String plugIn() {
         return refrigerator.startCooling();
     }
