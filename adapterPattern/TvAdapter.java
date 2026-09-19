900a6732a274d0d0601afa605e296de491aeda63
Joey Gerodias Jr. <101050559+JoeyGerodiasJr@users.noreply.github.com>
2026-09-19 18:12:34 +0800

    Implement plugIn method in TvAdapter

--- adapterPattern/TvAdapter.java
+++ adapterPattern/TvAdapter.java
@@ -27,4 +27,9 @@ public class TvAdapter implements  RemoteControllerFunction {
     public String pressMinusButton() {
         return tv.changeChannelDown();
     }
+
+    @Override
+    public String plugIn() {
+    return "TV is plugged into the power outlet.";
+    }
 }
