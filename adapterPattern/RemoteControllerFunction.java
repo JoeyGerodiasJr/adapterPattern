a30c70206fb235a3ded0e660ad21ff7ed7e4eaa2
Joey Gerodias Jr. <101050559+JoeyGerodiasJr@users.noreply.github.com>
2026-09-19 18:12:03 +0800

    Add plugIn method to RemoteControllerFunction interface

--- adapterPattern/RemoteControllerFunction.java
+++ adapterPattern/RemoteControllerFunction.java
@@ -5,4 +5,5 @@ public interface RemoteControllerFunction {
     public String powerOff();
     public String pressPlusButton();
     public String pressMinusButton();
+    public String plugIn();
 }
