4e9861742acbcd8b38846fa1baf075808f3937df
Joey Gerodias Jr. <101050559+JoeyGerodiasJr@users.noreply.github.com>
2026-09-19 18:11:34 +0800

    Integrate new devices with adapters
    Added adapters for new devices including Laptop, Refrigerator, and SmartphoneCharger.

--- adapterPattern/ApplianceApp.java
+++ adapterPattern/ApplianceApp.java
@@ -25,5 +25,17 @@ public class ApplianceApp {
         System.out.println(airConRc.pressPlusButton());
         System.out.println(speakerRc.pressPlusButton());
 
+         // Plug in New Devices
+        Laptop laptop = new Laptop();
+        RemoteControllerFunction laptopRc = new LaptopAdapter(laptop);
+        Refrigerator refrigerator = new Refrigerator();
+        RemoteControllerFunction refrigeratorRc = new RefrigeratorAdapter(refrigerator);
+        SmartphoneCharger smartphoneCharger = new SmartphoneCharger();
+        RemoteControllerFunction smartphoneRc = new SmartphoneAdapter(smartphoneCharger);
+        System.out.println();
+        System.out.println("Plugging devices into the power outlet:");
+        System.out.println(laptopRc.plugIn());
+        System.out.println(refrigeratorRc.plugIn());
+        System.out.println(smartphoneRc.plugIn());
     }
 }
